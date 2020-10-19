e8c902b5f2ab7132c79659ad2bf76eb7d299d799
Leonid Abasov <leonid.abasov@namecheap.com>
2020-10-19 16:32:21 +0300

    chore: 0.9.83

---
+++ PipelineLauncher.Extensions/Models/WrappedInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PipelineLauncher.Extensions.Models
+{
+    public class WrappedInput<TInput, TData>
+    {
+        public WrappedInput(TInput input, TData data)
+        {
+            Input = input;
+            Data = data;
+        }
+
+        public TInput Input { get; set; }
+
+        public TData Data { get; set; }
+    }
+}
---
+++ PipelineLauncher.Extensions/PipelineSetup/PipelineSetup.cs
@@ -0,0 +1,20 @@
+﻿using PipelineLauncher.Extensions.Models;
+using PipelineLauncher.PipelineSetup;
+
+namespace PipelineLauncher.Extensions.PipelineSetup
+{
+    public static class PipelineSetup
+    {
+        public static IPipelineSetup<TPipelineInput, (TPipelineOutput input, TData data)> WrapInputAsTuple<TPipelineInput, TPipelineOutput, TData>(this IPipelineSetup<TPipelineInput, TPipelineOutput> pipelineSetup, TData data) =>
+            pipelineSetup.Stage(input => (input, data));
+
+        public static IPipelineSetup<TPipelineInput, WrappedInput<TPipelineOutput, TData>> WrapInput<TPipelineInput, TPipelineOutput, TData>(this IPipelineSetup<TPipelineInput, TPipelineOutput> pipelineSetup, TData data) =>
+            pipelineSetup.Stage(input => new WrappedInput<TPipelineOutput, TData>(input, data));
+
+        public static IPipelineSetup<TPipelineInput, TPipelineOutput> UnwrapInput<TPipelineInput, TPipelineOutput, TData>(this IPipelineSetup<TPipelineInput, (TPipelineOutput input, TData data)> pipelineSetup) =>
+            pipelineSetup.Stage(wrappedInput => wrappedInput.input);
+
+        public static IPipelineSetup<TPipelineInput, TPipelineOutput> UnwrapInput<TPipelineInput, TPipelineOutput, TData>(this IPipelineSetup<TPipelineInput, WrappedInput<TPipelineOutput, TData>> pipelineSetup) =>
+            pipelineSetup.Stage(wrappedInput => wrappedInput.Input);
+    }
+}
